a8e34b7eed64804024123740916584902b6eba48
VR <vr@VRs-MacBook-Air.local>
2019-09-04 14:50:52 +0200

    Redesigned

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,22 @@
 const gulp = require("gulp");
 const del = require("del");
+const plumber = require('gulp-plumber');
+const browserSync = require("browser-sync");
+
 // Styles
 const sass = require("gulp-sass");
 const prefix = require("gulp-autoprefixer");
 const minify = require("gulp-cssnano");
-const header = require("gulp-header");
-const browserSync = require("browser-sync");
+const cleanCss = require('gulp-clean-css');
+
 // Scripts
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
-const PATHS = {
-  input: "src/",
-  output: "dist/",
-  scripts: {
-    input: "src/js/*",
-    polyfills: ".polyfill.js",
-    output: "dist/js/"
-  },
-  styles: {
-    input: "src/scss/**/*.{scss,sass}",
-    output: "dist/css/"
-  },
-  svgs: {
-    input: "src/svg/*.svg",
-    output: "dist/svg/"
-  },
-  copy: {
-    input: "src/copy/*",
-    output: "dist/"
-  },
-  reload: "./dist/"
-};
-
-gulp.task("clean", function clean() {
-  return del(["dist"]);
-});
-
-gulp.task("style", function style() {
-  return gulp
-    .src(PATHS.styles.input)
-    .pipe(sass())
-    .pipe(gulp.dest(PATHS.styles.output))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("style:prod", function style() {
-  return gulp
-    .src(PATHS.styles.input)
-    .pipe(sass())
-    .pipe(
-      prefix({
-        browsers: ["last 2 version", "> 0.25%"],
-        cascade: true,
-        remove: true
-      })
-    )
-    .pipe(
-      minify({
-        discardComments: {
-          removeAll: true
-        }
-      })
-    )
-    .pipe(header(BANNER.full, { package: package }))
-    .pipe(gulp.dest(PATHS.styles.output));
-});
-
-gulp.task("serve", function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-
-  gulp.watch("*.html").on("change", browserSync.reload);
-  gulp
-    .watch(PATHS.styles.input)
-    .on("change", gulp.series(["style"], browserSync.reload));
-});
-
-gulp.task("copyFiles", function() {
-  return gulp.src(PATHS.copy.input).pipe(gulp.dest(PATHS.copy.output));
-});
-
+// HTML
+const header = require("gulp-header");
+const fileinclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
 const package = require("./package.json");
 const BANNER = {
   full:
@@ -104,3 +39,105 @@ const BANNER = {
     " | <%= package.repository.url %>" +
     " */\n"
 };
+
+const PATHS = {
+  input: "src/",
+  output: "./dist/",
+  scripts: {
+    input: "src/js/*",
+  },
+  styles: {
+    input: "./src/scss/**/*.scss",
+  },
+  html: {
+    input: "src/pages/*.html",
+    watch: "./src/pages/**/*.html"
+  },
+  images: {
+    input: "src/images/**/*",
+  },
+  reload: "./dist/"
+};
+
+
+gulp.task("clean", function () {
+  return del([ "dist" ]);
+});
+
+gulp.task("fileinclude", function (cb) {
+  gulp.src(PATHS.html.input)
+    .pipe(plumber())
+    .pipe(fileinclude({
+      prefix: '#',
+      basepath: './src/pages'
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist'));
+  cb();
+});
+
+gulp.task("css:dev", function () {
+  return gulp
+    .src(PATHS.styles.input)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest(PATHS.output))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("css:prod", function () {
+  return gulp
+    .src(PATHS.styles.input)
+    .pipe(sass())
+    .pipe(cleanCss())
+    .pipe(
+      prefix({
+        browsers: [ "last 2 version", "> 0.25%" ],
+        cascade: true,
+        remove: true
+      })
+    )
+    .pipe(
+      minify({
+        discardComments: {
+          removeAll: true
+        }
+      })
+    )
+    .pipe(header(BANNER.full, { package: package }))
+    .pipe(gulp.dest(PATHS.output));
+});
+
+gulp.task("js:prod", function () {
+  return gulp
+    .src(PATHS.scripts.input)
+    .pipe(babel({
+      presets: [ '@babel/env' ]
+    }))
+    .pipe(uglify())
+    .pipe(header(BANNER.full, { package: package }))
+    .pipe(gulp.dest(PATHS.output))
+})
+
+gulp.task("watch", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  });
+
+  gulp.watch("*.html").on("change", browserSync.reload);
+  gulp.watch(PATHS.styles.input).on("change", gulp.series([ "css:dev" ], browserSync.reload));
+  gulp.watch(PATHS.scripts.input).on("change", gulp.series([ "js:prod" ], browserSync.reload));
+  gulp.watch(PATHS.images.input).on("change", gulp.series([ "copyImages" ], browserSync.reload));
+  gulp.watch(PATHS.html.watch).on('change', gulp.series([ "fileinclude" ], browserSync.reload));
+});
+
+gulp.task("copyImages", function () {
+  return gulp.src(PATHS.images.input).pipe(gulp.dest(PATHS.output + '/images/'));
+});
+
+gulp.task("build", function (cb) {
+  gulp.series("css:prod");
+  cb();
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,4 @@
-window.addEventListener("DOMContentLoaded", event => {
-  const body = document.querySelector("body");
-
-  (localStorage.getItem("mode") || "dark") === "night"
-    ? body.classList.add("night")
-    : body.classList.remove("night");
-
-  const themeSwitcher = document.getElementById("theme-switcher");
-
-  if (themeSwitcher) {
-    themeSwitcher.addEventListener("click", () => {
-      body.classList.toggle("night");
-    });
-  }
-
+window.addEventListener('load', function () {
   console.log(
     "%c Hi👋🏼 Looking for something or someone ? 👀 ",
     "font-size: 1.5rem; font-weight: bold;"
